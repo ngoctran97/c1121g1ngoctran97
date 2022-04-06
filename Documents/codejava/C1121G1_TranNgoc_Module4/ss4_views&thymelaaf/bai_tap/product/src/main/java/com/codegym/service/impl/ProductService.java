99aa4ec3b8c7dcd1c2422cd4015259d981b1545c
ngoctran97 <91454475+ngoctran97@users.noreply.github.com>
2022-04-06 16:49:07 +0700

    up bai ss4

--- Documents/codejava/C1121G1_TranNgoc_Module4/ss4_views&thymelaaf/bai_tap/product/src/main/java/com/codegym/service/impl/ProductService.java
+++ Documents/codejava/C1121G1_TranNgoc_Module4/ss4_views&thymelaaf/bai_tap/product/src/main/java/com/codegym/service/impl/ProductService.java
@@ -12,13 +12,14 @@ import java.util.Map;
 @Service
 public class ProductService implements IProductService {
 
-    private static final Map<Integer , Product> productList;
+    private static final Map<Integer, Product> productList;
+
     static {
 
         productList = new HashMap<>();
-        productList.put(1,new Product(1,"Iphone 3",500,"blue","Iphone"));
-        productList.put(2,new Product(2,"Iphone 5",600,"blue","Iphone"));
-        productList.put(3,new Product(3,"Iphone 7",700,"blue","Iphone"));
+        productList.put(1, new Product(1, "Iphone 3", 500, "blue", "Iphone"));
+        productList.put(2, new Product(2, "Iphone 5", 600, "blue", "Iphone"));
+        productList.put(3, new Product(3, "Iphone 7", 700, "blue", "Iphone"));
     }
 
     @Override
@@ -33,7 +34,13 @@ public class ProductService implements IProductService {
 
     @Override
     public Object findById(int id) {
-        return productList.get(id);
+        List<Product> productList = new ArrayList<>();
+        for (Product p : productList) {
+            if (p.getId() == id) {
+                return productList.get(id);
+            }
+        }
+        return null;
 
     }
 
@@ -51,8 +58,8 @@ public class ProductService implements IProductService {
     public List<Product> searchName(String name) {
         List<Product> products = new ArrayList<>(productList.values());
         List<Product> pro = new ArrayList<>();
-        for (Product p:products){
-            if(p.getName().contains(name)){
+        for (Product p : products) {
+            if (p.getName().contains(name)) {
                 pro.add(p);
             }
         }
